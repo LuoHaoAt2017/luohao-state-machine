ff2a4fe7866b5beaee33af5bf82ef9197c7d9faa
luohao <luohao@authine.com>
2020-11-23 12:12:57 +0800

    test: 搭建测试环境

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+
+module.exports = {
+  moduleFileExtensions: [
+    "js",
+    "ts",
+  ],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  testEnvironment: "node",
+  testMatch: [
+    "**/__tests__/**/*.[jt]s?(x)",
+    "**/?(*.)+(spec|test).[tj]s?(x)"
+  ],
+  testPathIgnorePatterns: [
+    "/node_modules/"
+  ],
+  transform: {
+    "^.+\\.ts$": "<rootDir>/node_modules/ts-jest",
+    "^.+\\.js$": "<rootDir>/node_modules/babel-jest",
+  },
+  verbose: true,
+  watchman: true,
+};
---
+++ test/index.spec.js
@@ -0,0 +1,7 @@
+import { describe, expect, test } from "@jest/globals";
+
+describe('hello', () => {
+  test('should ', () => {
+    expect(2).toBe(2);
+  });
+});
