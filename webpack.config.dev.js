dc67f21f7356dd9be9c32b1068aff12977a7ca5c
LuoHaoAt2017 <hluo5206@gmail.com>
2021-08-24 00:43:14 +0800

    build

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,12 +1,6 @@
 const path = require('path');
 const { merge } = require('webpack-merge');
 const config = require('./webpack.config');
-const { VueLoaderPlugin } = require('vue-loader');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-function resolve(x) {
-  return path.resolve(__dirname, x)
-}
 
 module.exports = merge(config, {
   mode: 'development',
@@ -15,33 +9,6 @@ module.exports = merge(config, {
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist')
   },
-  module: {
-    rules: [
-      {
-        test: /\.vue/,
-        use: ['vue-loader'],
-        include: resolve('./example')
-      },
-      {
-        test: /\.(css|less)$/,
-        use: ['vue-style-loader', 'css-loader', 'less-loader'],
-        include: resolve('./example')
-      },
-      {
-        test: /\.(png|svg|jpg)$/,
-        use: ['file-loader'],
-        include: resolve('./example')
-      }
-    ]
-  },
-  plugins: [
-    new VueLoaderPlugin(),
-    new HtmlWebpackPlugin({
-      title: 'luohao-formlogic',
-      template: resolve('./public/index.html'),
-      favicon: resolve('./public/favicon.ico'),
-    })
-  ],
   devServer: {
     port: 9000,
     open: true,
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,10 @@
 const path = require('path');
+const { VueLoaderPlugin } = require('vue-loader');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+function resolve(x) {
+  return path.resolve(__dirname, x)
+}
 
 module.exports = {
   module: {
@@ -6,18 +12,35 @@ module.exports = {
       {
         test: /\.js/,
         use: ['babel-loader'],
-        exclude: /node_modules/,
       },
+      {
+        test: /\.vue/,
+        use: ['vue-loader'],
+      },
+      {
+        test: /\.(css|less)$/,
+        use: ['vue-style-loader', 'css-loader', 'less-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg)$/,
+        use: ['file-loader'],
+      }
     ]
   },
+  plugins: [
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'luohao-formlogic',
+      template: resolve('./public/index.html'),
+      favicon: resolve('./public/favicon.ico'),
+    })
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
     },
     extensions: ['.js', '.ts', '.vue', '.json']
   },
-  plugins: [
-  ],
   externals: {
   }
 }
