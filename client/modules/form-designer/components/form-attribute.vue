99164aff909c7fec9c64bfa4df5063c33660dc7d
LuoHaoAt2017 <hluo5206@gmail.com>
2021-09-05 12:22:21 +0800

    feat: 表单属性和控件属性

---
+++ client/modules/form-designer/components/form-attribute.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="form-attribute">
+    
+  </div>
+</template>
+<script>
+import {
+  Button,
+  Icon,
+  Modal,
+  Row,
+  Col,
+  Input,
+} from "ant-design-vue";
+export default {
+  name: 'form-attribute',
+  components: {
+    AButton: Button,
+    AIcon: Icon,
+    ACol: Col,
+    ARow: Row,
+  },
+}
+</script>
+<style lang="less" scoped>
+.form-attribute {
+  padding: 10px;
+}
+</style>
